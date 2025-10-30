7e26a34aa258a48cba5cd99e6f0cb0b56abc47f9
InesCabBet <icb3rg@gmail.com>
2025-10-30 16:07:24 +0200

    haskell 3 start

---
+++ FUNCTIONAL_PROGRAMMING/ExerciseSet3.hs
@@ -0,0 +1,10 @@
+{-
+  VILNIUS UNIVERSITY
+  FUNCTIONAL PROGRAMMING
+  @author Inés Cabrera Betancor
+  @initialDate 20 OCT 2025
+  @finalDate   NOV 2025
+  @brief Third assignment using haskell and structures and function application
+-}
+
+module ExerciseSet3 where
