320d5a48cb0094e430337958c6740a03b4d5185e
InesCabBet <icb3rg@gmail.com>
2025-11-06 17:09:55 +0200

    trim function

--- ARTIFICIAL_INTELLIGENCE/ASSIGNMENT5-ForwardChaining/forwardChaining.cc
+++ ARTIFICIAL_INTELLIGENCE/ASSIGNMENT5-ForwardChaining/forwardChaining.cc
@@ -8,3 +8,21 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <vector>
+#include <set>
+#include <string>
+#include <algorithm>
+
+/**
+ * @brief Function to trim a string
+ * @param str string to trim
+ */
+std::string trim(const std::string &str) {
+  size_t primero = str.find_first_not_of(" \t\n\r");
+  if (primero == std::string::npos) {
+    return "";
+  }
+  size_t ultimo = str.find_last_not_of(" \t\n\r");
+  return str.substr(primero, ultimo - primero + 1);
+}
