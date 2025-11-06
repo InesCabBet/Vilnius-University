a6211b8e630556a3ce90b186fd416c69bceca6d8
InesCabBet <icb3rg@gmail.com>
2025-11-06 18:07:02 +0200

    comments

--- ARTIFICIAL_INTELLIGENCE/ASSIGNMENT5-ForwardChaining/Rule.cc
+++ ARTIFICIAL_INTELLIGENCE/ASSIGNMENT5-ForwardChaining/Rule.cc
@@ -6,13 +6,9 @@
  * @brief Assignment 5 - Forward Chaining - Rule Class
  */
 
-#include <iostream>
-#include <fstream>
-#include <sstream>
 #include <vector>
 #include <set>
 #include <string>
-#include <algorithm>
 
 #ifndef __RULE__
 #define __RULE__
@@ -26,6 +22,13 @@ class Rule {
     bool flag2;
     Rule(std::string _name, std::vector<std::string> _lhs, std::string _rhs) : name(_name), lhs(_lhs), rhs(_rhs), flag1(false), flag2(false) {} 
     Rule(std::string _name, std::string _lhs, std::string _rhs) : name(_name), lhs({_lhs}), rhs(_rhs), flag1(false), flag2(false) {}
+    /**
+     * @brief checks if the rule can be applied given known facts. A rule can be applied if all facts on the 
+     * left side are present in the current set of facts
+     * @param facts set of current facts
+     * @return true if all facts necessary are in the set
+     * @return false if not all facts necessary are in the set
+     */
     bool canApply(const std::set<std::string> & facts) const {
       for (const auto &fact : lhs) {
         if (facts.find(fact) == facts.end()) {
@@ -34,9 +37,17 @@ class Rule {
       }
       return true;
     }
+    /**
+     * @brief Resets the state of flag 1 to false
+     */
     void resetFlag1() {
       flag1 = false;
     }
+    /**
+     * @brief Returns a text representation of the rule
+     * Format: "name: fact1, fact2 -> result"
+     * @return std::string string that represents the rule
+     */
     std::string toString() const {
       std::string result = name + ": ";
       for (size_t i = 0; i < lhs.size(); i++) {
@@ -48,6 +59,12 @@ class Rule {
       result += " -> " + rhs;
       return result;
     }
+    /**
+     * @brief Obtains the facts we would need to apply the rule
+     * It goes through the lhs facts and returns those who are not present in the current fact set
+     * @param facts current fact set
+     * @return a list of remaining facts
+     */
     std::vector<std::string> getMissingFacts(const std::set<std::string> &facts) const {
       std::vector<std::string> missing;
       for (const auto &fact : lhs) {
