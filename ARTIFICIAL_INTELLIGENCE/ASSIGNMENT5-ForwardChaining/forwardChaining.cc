c6c5a39c263ac224931bb423a1686e4d8a9a07b1
InesCabBet <icb3rg@gmail.com>
2025-11-06 18:29:50 +0200

    main + fc class

--- ARTIFICIAL_INTELLIGENCE/ASSIGNMENT5-ForwardChaining/forwardChaining.cc
+++ ARTIFICIAL_INTELLIGENCE/ASSIGNMENT5-ForwardChaining/forwardChaining.cc
@@ -7,34 +7,22 @@
  */
 
 #include "Rule.cc"
+#include <sstream>
 
-/**
- * @brief Function to trim a string
- * @param str string to trim
- */
-std::string trim(const std::string &str) {
-  size_t primero = str.find_first_not_of(" \t\n\r");
-  if (primero == std::string::npos) {
-    return "";
-  }
-  size_t ultimo = str.find_last_not_of(" \t\n\r");
-  return str.substr(primero, ultimo - primero + 1);
-}
+#ifndef __FORWARD__
+#define __FORWARD__
 
-/**
- * @brief Functions to split a string
- * @param str string to split
- * @param delimiter where to stop to trim
- */
-std::vector<std::string> split (const std::string &str, char delimiter) {
-  std::vector<std::string> elems;
-  std::stringstream ss(str);
-  std::string elem;
-  while (getline(ss, elem, delimiter)) {
-    std::string trimed = trim(elem);
-    if (!trimed.empty()) {
-      elems.push_back(trimed);
-    }
-  }
-  return elems;
-}
+class ForwardChaining {
+  private: 
+  std::vector<Rule> rules;
+  std::set<std::string> facts;
+  std::string goal;
+  std::vector<std::string> path;
+  std::vector<std::string> trace;
+  std::string studentInfo;
+  std::string testName;
+  
+};
+
+#endif
+
