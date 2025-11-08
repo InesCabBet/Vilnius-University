b48c6afb4fccd3d8a375969308aeaabc7ca83e77
InesCabBet <icb3rg@gmail.com>
2025-11-08 17:07:21 +0200

    utils.h

--- ARTIFICIAL_INTELLIGENCE/ASSIGNMENT6-BackwardChaining/src/utils.h
+++ ARTIFICIAL_INTELLIGENCE/ASSIGNMENT6-BackwardChaining/src/utils.h
@@ -33,4 +33,35 @@ class Rule {
     std::string toString() const;
 };
 
+class BackwardChaining {
+  private:
+    std::vector<Rule> rules;
+    std::set<std::string> initialFacts;
+    std::vector<std::string> initialFactsOrdered;
+    std::string goal;
+    std::vector<std::string> finalPath;
+    std::vector<std::string> trace;
+    int traceCounter;
+    std::set<std::string> memoTrue;
+    std::unordered_map<std::string, std::vector<std::string>> memoProofTrue;
+    std::vector<std::string> inferredOrder;
+    std::string getIndent(int depth) const {return std::string(depth, '-');}
+    std::vector<std::string> buildFactsList(const std::string &current, const std::string &type) const;
+    bool proveGoal(const std::string &currentGoal, int depth, std::set<std::string> &inProgress, std::vector<std::string> &outPath);
+  
+  public:
+    BackwardChaining (std::vector<Rule> r, std::vector<std::string> facts, std::string g) : rules(std::move(r)), goal(std::move(g)), traceCounter(1) {
+      initialFacts.insert(facts.begin(), facts.end());
+      initialFactsOrdered.assign(initialFacts.begin(), initialFacts.end());
+      sort(initialFactsOrdered.begin(), initialFactsOrdered.end());
+    }
+    bool solve();
+    void printResults();
+};
+
+class FileParser {
+  public: 
+    static bool parseFile(const std::string &filename, std::vector<Rule> &rules, std::vector<std::string> &facts, std::string &goal);
+};
+
 #endif
