7edf356a1e5eb875b12875be88e82d184312bb45
InesCabBet <icb3rg@gmail.com>
2025-12-09 12:30:12 +0200

    emepzar hs 4

---
+++ FUNCTIONAL_PROGRAMMING/ExerciseSet4.hs
@@ -0,0 +1,12 @@
+{-
+  VILNIUS UNIVERSITY
+  FUNCTIONAL PROGRAMMING
+  @author Inés Cabrera Betancor
+  @initialDate 03 DEC 2025
+  @finalDate xx DEC 2025
+  @brief Fourth assignment using haskell and structures and function application
+-}
+
+module ExerciseSet4 where
+
+--- Exercise 1 -> Trees
