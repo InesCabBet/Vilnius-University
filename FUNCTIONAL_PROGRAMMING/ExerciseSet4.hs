c91fc137a3d37454be3e45d1016b5d854ea28600
InesCabBet <icb3rg@gmail.com>
2025-12-11 19:14:05 +0200

    ejercicio 1 practica 4

--- FUNCTIONAL_PROGRAMMING/ExerciseSet4.hs
+++ FUNCTIONAL_PROGRAMMING/ExerciseSet4.hs
@@ -10,3 +10,16 @@
 module ExerciseSet4 where
 
 --- Exercise 1 -> Trees
+data GTree a = Leaf a | Gnode [GTree a]
+
+depth :: GTree a -> Int 
+depth (Leaf _) = 1
+depth (Gnode xs) = 1 + maximum (map depth xs)
+
+occurs :: Eq a => a -> GTree a -> Bool
+occurs x (Leaf y) = x == y
+occurs x (Gnode xs) = any (occurs x) xs
+
+mapGTree :: (a -> b) -> GTree a -> GTree b
+mapGTree f (Leaf x) = Leaf (f x)
+mapGTree f (Gnode xs) = Gnode (map (mapGTree f) xs)
